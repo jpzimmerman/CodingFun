044fd77abe53db0f83a7dcdd196c4117afbc6bf8
Zimmerman <joshuazimmerman@quickenloans.com>
2018-12-27 16:56:45 -0500

    This is my code

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            from DayOfWeek s in typeof(DayOfWeek).GetEnumValues().AsQueryable()
+            where (int)s == 7
+            select 
+
+            foreach (DayOfWeek s in typeof(DayOfWeek).GetEnumValues())
+            {
+                
+                typeof(DayOfWeek).GetEnumValues().AsQueryable()
+                Console.WriteLine(s);
+            }
+
+            while (true) { }
+
+        }
+    }
+}
